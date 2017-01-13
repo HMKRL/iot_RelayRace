0b608622d84f3c3e418ac7ec2e1c2ce46a93e4f4
Yu-Cheng Liang <a135246a135246@gmail.com>
2017-01-13 13:43:43 +0800

    Reset park location

--- RelayRace.cpp
+++ RelayRace.cpp
@@ -46,7 +46,7 @@ bool RelayRace::askRFID(uint8_t *sn, bool debug) {
 		return true;
 	}
 
-	if(ID == ID2 && msg.buffer[6] == MAP_PARK_4) {
+	if(ID == ID2 && msg.buffer[6] == MAP_PARK_2) {
 		wifi->complete();
 		wifi->sendToClient(LEADER_ID, "FINISH");
 
@@ -64,7 +64,7 @@ bool RelayRace::askRFID(uint8_t *sn, bool debug) {
 		return true;
 	}
 
-	if(ID == LEADER_ID && msg.buffer[6] == MAP_PARK_2) {
+	if(ID == LEADER_ID && msg.buffer[6] == MAP_PARK_4) {
 		wifi->complete();
 		return true;
 	}
